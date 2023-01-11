91e9d4808f94a21204f287f64dd4880360715c5b
Marco Sousa-Poza <marco.sousapoza@gmail.com>
2023-01-11 17:52:03 +0100

    soeplong creation

--- weather/src/helper.py
+++ weather/src/helper.py
@@ -1,22 +1,110 @@
-from sqlalchemy import create_engine
 import geopandas as gpd
 import pandas as pd
-import numpy as np
-from meteostat import Stations, Daily, Point
-from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
+from tqdm.notebook import tqdm
 
-def get_nuts_data(lvl:int) -> pd.DataFrame:
-    data = gpd.read_file(f"./data/nuts5000/5000_NUTS{lvl}.shp")
-    # Calculate centroids
-    ct = data.to_crs(epsg=3035).centroid.to_crs(epsg=4326)
-    X = ct.map(lambda p: p.x) * 100
-    Y = ct.map(lambda p: p.y) * 100
-    X = X.astype(int)
-    Y = Y.astype(int)
-    centroids = pd.DataFrame({'lon_times100':X, 'lat_times100':Y})
-    nuts = data[['NUTS_LEVEL', 'NUTS_CODE', 'NUTS_NAME']]
-    return pd.concat([nuts, centroids], axis=1)
+##########################
+# READING DATA FUNCTIONS #
+##########################
+
+def get_station_data(dirpath:str, ids:list[str], bar=True) -> pd.DataFrame:
+
+    # the concatonated dataframe variable
+    rdf = pd.DataFrame()
+
+    # create progress bar
+    if bar:
+        pbar = tqdm(ids, leave=False)
+    else:
+        pbar = ids
+
+    # read files
+    for i in pbar:
+        filename = f'{dirpath}/{i}.csv'
+
+        # set discription of progress bar
+        if bar:
+            pbar.set_description(f"Reading {filename}")
+        new = pd.read_csv(filename, converters={'id': str})
+
+        # read file
+        if rdf.empty:
+            rdf = new
+        else:
+            rdf = pd.concat([rdf, new], ignore_index=True, axis=0)
+    
+    rdf['time'] = pd.to_datetime(rdf['time'])
+    return rdf
+
+
+def get_nuts_weather_data(infofile:str, dirpath:str, nutslvl:int, code:str=None, bar=True) -> pd.DataFrame:
+    
+    # read the station info
+    stations = gpd.read_file(infofile, converters={'id': str})
+    stations['id'] = stations['id'].astype(str)
+
+    # if code is specified only data from that area is selected
+    if code:
+        stations = stations[stations[f'NUTS_CODE_{nutslvl}'] == code]
+
+    if stations.empty:
+        raise ValueError("The parameters you gave do not give any results. Check them")
+
+    # read station data
+    ids = stations['id'].tolist()
+    data = get_station_data(dirpath, ids, bar=bar)
+    data['id'] = data['id'].astype(str)
+
+    # join the dataframes
+    joined:pd.DataFrame
+    joined = stations.merge(data, on='id', how='right')
+
+    # group by time and nuts level and then take the average
+    result = joined.groupby([f'NUTS_CODE_{nutslvl}', 'time']).mean()
+
+    # return the result
+    return result
+
+
+def get_point_weather_data(point:tuple[float, float], max_distance:float|int) -> pd.DataFrame:
+    #TODO test this function!!!
+    #TODO make function such that a vector of points can be used.
+
+    # read the station info
+    stations:gpd.GeoDataFrame
+    stations = gpd.read_file('./prod/stationinfo.geojson')
+
+    # calculate distance of each station to point
+    stations['distance'] = stations[['geometry']].distance(point)
+
+    # filter stations that are too far away
+    stations = stations[stations['distance'] <= max_distance]
+
+    # calculate weight
+    stations['weight'] = 1 / stations['distance']
+
+    # get the station data
+    data = get_station_data(list(stations['id']))
+    data.join(stations, on='id', how='left')
+    
+    # group all the stations by date
+    grouped = data.groupby('time')
+
+    # compute the weighted average
+    #create the custom weighted mean aggregation function
+    value_cols = 'tavg tmin tmax prcp snow wdir wspd wpgt pres tsun'.split() # values to aggregate
+    wm = lambda x, w: np.average(x, weights=w)
+    agg_funcs = {col: wm for col in value_cols}
+    result = grouped[[*value_cols, 'weight']].aggregate(agg_funcs)
+
+    # return the result
+    return result
+
+
+######################
+# PLOTTING FUNCTIONS #
+######################
 
 def plot_centroids_with_radius(lvl:int):
     data = gpd.read_file(f"./data/nuts5000/5000_NUTS{lvl}.shp")
@@ -35,6 +123,8 @@ def plot_centroids_with_radius(lvl:int):
     ax.legend()
     plt.show()
 
+
+"""
 def get_daily_weather_data(
     lat:float, lon:float, 
     radius:int, start:datetime, 
@@ -63,3 +153,4 @@ def get_daily_weather_data_loc(
     data = daily.normalize().interpolate().aggregate('1D', spatial=True).fetch()
     # Return data
     return data
+"""
